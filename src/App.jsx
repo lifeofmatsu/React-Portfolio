3a8e14d813b34d75db8a311a7bbd71b5e9198859
Jus Ferrell <j1ferrel@ucsd.edu>
2024-04-11 18:01:46 -0700

    working draft

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,41 @@
-import { useState } from 'react';
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import About from './components/About';
-import Contact from './components/Contact';
-import Resume from './components/Resume';
-import Portfolio from './components/Portfolio';
+import React, { useState, useRef } from 'react';
+import Header from './components/Header.jsx';
+import AboutMe from './components/AboutMe.jsx';
+import Portfolio from './components/Portfolio.jsx';
+import Contact from './components/Contact.jsx';
+import Resume from './components/Resume.jsx';
+import Footer from './components/Footer.jsx';
 
-function App() {
-	const [currentPage, setCurrentPage] = useState("About");
-    // return the correct component to render.
-    const renderPage = () => {
-        if (currentPage === 'About') {
-          return <About />;
+const App = () => {
+    const aboutMeRef = useRef(null);
+    const portfolioRef = useRef(null);
+    const contactRef = useRef(null);
+    const resumeRef = useRef(null);
+
+    const scrollToSection = (sectionRef) => {
+        if (sectionRef && sectionRef.current) {
+            sectionRef.current.scrollIntoView({ behavior: 'smooth' });
         }
-        if (currentPage === 'Portfolio') {
-          return <Portfolio />;
-        }
-        if (currentPage === 'Contact') {
-            return <Contact />;
-        }
-        if (currentPage === 'Resume') {
-            return <Resume />;
-        }
-        return null
     };
-    
-      const handlePageChange = (page) => setCurrentPage(page);
-    
-      return (
-        <div>
-            <Header currentPage={currentPage} handlePageChange={handlePageChange} 
-            ></Header>
-            <div>
-                <main>{renderPage()}</main>
-            </div>
-            <div>
-				<Footer></Footer>
-			</div>
-        </div>
-      );
-    }
 
+    return (
+        <>
+            <Header
+                scrollToSection={scrollToSection}
+                aboutMeRef={aboutMeRef}
+                portfolioRef={portfolioRef}
+                contactRef={contactRef}
+                resumeRef={resumeRef}
+            />
+            <main>
+                <section ref={aboutMeRef}><AboutMe /></section>
+                <section ref={portfolioRef}><Portfolio /></section>
+                <section ref={contactRef}><Contact /></section>
+                <section ref={resumeRef}><Resume /></section>
+            </main>
+            <Footer />
+        </>
+    );
+}
 
 export default App;
