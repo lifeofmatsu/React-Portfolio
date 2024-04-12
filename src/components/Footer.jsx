cbfc48086942ac07f088120cbfaf10b07dc42175
Jus Ferrell <j1ferrel@ucsd.edu>
2024-04-11 21:52:27 -0700

    edited filepaths

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ const Footer = () => {
 	return (
 		<footer>
 			<a href='https://github.com/lifeofmatsu'>
-				<img src='src/assets/github-white.png' alt='GitHub' />
+				<img src='/github-white.png' alt='GitHub' />
 			</a>
 			<a href='https://www.linkedin.com/in/lifeofmatsu/'>
-				<img src='src/assets/linkedin-white.png' alt='LinkedIn' />
+				<img src='/linkedin-white.png' alt='LinkedIn' />
 			</a>
 		</footer>
 	);
