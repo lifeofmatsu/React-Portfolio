799ce0d2d272ec9363654df0dc885e64eef058cb
Jus Ferrell <j1ferrel@ucsd.edu>
2024-04-20 00:35:45 -0700

    layout restructuring and tailwind theme implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,41 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Header from './components/Header.jsx';
-import AboutMe from './components/AboutMe.jsx';
+import About from './components/About.jsx';
+import Experience from './components/Experience.jsx';
 import Portfolio from './components/Portfolio.jsx';
 import Contact from './components/Contact.jsx';
-import Resume from './components/Resume.jsx';
 import Footer from './components/Footer.jsx';
+import { scrollToSection, toggleTheme } from './utils/helpers';
+
 
 const App = () => {
-    const aboutMeRef = useRef(null);
+    const [theme, setTheme] = useState('dark');
+
+    const aboutRef = useRef(null);
+    const experienceRef = useRef(null);
     const portfolioRef = useRef(null);
     const contactRef = useRef(null);
-    const resumeRef = useRef(null);
 
-    const scrollToSection = (sectionRef) => {
-        if (sectionRef && sectionRef.current) {
-            sectionRef.current.scrollIntoView({ behavior: 'smooth' });
-        }
-    };
+    // allow user to toggle light or dark theme
 
     return (
-        <>
+        <div className={theme}>
             <Header
                 scrollToSection={scrollToSection}
-                aboutMeRef={aboutMeRef}
+                aboutMeRef={aboutRef}
                 portfolioRef={portfolioRef}
                 contactRef={contactRef}
-                resumeRef={resumeRef}
+                resumeRef={experienceRef}
             />
             <main>
-                <section ref={aboutMeRef}><AboutMe /></section>
+                <section ref={aboutRef}><About /></section>
+                <section ref={experienceRef}><Experience /></section>
                 <section ref={portfolioRef}><Portfolio /></section>
                 <section ref={contactRef}><Contact /></section>
-                <section ref={resumeRef}><Resume /></section>
             </main>
             <Footer />
-        </>
+            <button onClick={toggleTheme(setTheme)}>Toggle Theme</button>
+        </div>
     );
 }
 
