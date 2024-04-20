e0e8e0c285c302e93c0620d8cad8d4d417057e0e
Jus Ferrell <j1ferrel@ucsd.edu>
2024-04-20 00:37:02 -0700

    href scroll behavior and theme toggling functions

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,15 @@
+// scroll behavior when Nav link is clicked
+export const scrollToSection = (sectionRef) => {
+    if (sectionRef && sectionRef.current) {
+        sectionRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+};
+
+// allow user to toggle light or dark theme
+export const toggleTheme = (setTheme) => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+}
+
 // Email validation function
 export const isValidEmail = (email) => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
